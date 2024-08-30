68c0bb1de0ff5dfbed6bf28cadeb9855127f6e88
sinasll <sinasalah6@gmail.com>
2024-08-30 17:30:53 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const playButton = document.getElementById('play-button');
-    
-    playButton.addEventListener('click', function() {
-        alert('Starting the game...');
-    });
-});
