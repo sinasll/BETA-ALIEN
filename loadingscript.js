74bc3b2f636053b055f0e9697fdbe91c6a80832b
sinasll <sinasalah6@gmail.com>
2024-09-02 16:22:26 +0300

    Update loadingscript.js

--- loadingscript.js
+++ loadingscript.js
@@ -1,5 +1,5 @@
 // Optional: Add a timed delay to transition from the loading page
 setTimeout(() => {
     // Redirect to your game's main page or home screen after loading
-    window.location.href = "main.html"; // Replace with the actual main game URL
-}, 3000); // 3-second delay
+    window.location.href = "gamestation.html"; // Replace with the actual main game URL
+}, 5000); // Delay before redirect, adjust as needed
