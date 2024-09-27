a5efd897a800b129a2b1252dc0834cd2e58ff94c
sinasll <sinasalah6@gmail.com>
2024-09-27 14:19:22 +0300

    Delete loadingscript.js

--- loadingscript.js
+++
@@ -1,5 +0,0 @@
-// Optional: Add a timed delay to transition from the loading page
-setTimeout(() => {
-    // Redirect to your game's main page or home screen after loading
-    window.location.href = "gamestation.html"; // Replace with the actual main game URL
-}, 5000); // Delay before redirect, adjust as needed
