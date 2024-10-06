efde095db7a452618df264eb75eeb5a54c5f09e6
sinasll <sinasalah6@gmail.com>
2024-10-06 18:11:52 +0300

    Update loadingscript.js

--- loadingscript.js
+++ loadingscript.js
@@ -1,12 +1,3 @@
-// Initialize Telegram WebApp
-Telegram.WebApp.ready();
-
-// Request the app to expand to full size
-Telegram.WebApp.expand();
-
-// Ensure the app fits the full viewport height dynamically
-document.documentElement.style.height = Telegram.WebApp.viewportHeight + 'px';
-
 // Optional: Add a timed delay to transition from the loading page
 setTimeout(() => {
     // Redirect to your game's main page or home screen after loading
