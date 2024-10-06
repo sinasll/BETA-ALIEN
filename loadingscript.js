4a4ee211fb24006e9be61265063f5f8cd685ad62
sinasll <sinasalah6@gmail.com>
2024-10-06 15:15:12 +0300

    Update loadingscript.js

--- loadingscript.js
+++ loadingscript.js
@@ -3,3 +3,10 @@ setTimeout(() => {
     // Redirect to your game's main page or home screen after loading
     window.location.href = "main.html"; // Replace with the actual main game URL
 }, 5000); // Delay before redirect, adjust as needed
+// Initialize Telegram WebApp
+Telegram.WebApp.ready();
+
+// Request the app to expand to full size
+Telegram.WebApp.expand();
+// Ensure the app fits the full viewport height dynamically
+document.documentElement.style.height = Telegram.WebApp.viewportHeight + 'px';
